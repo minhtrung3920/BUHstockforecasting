5c0aa798861249c5bd2e046d358f90d3ca7545af
Trung Thanh Luu <thanh.187pm14010@vanlanguni.vn>
2021-09-30 23:38:09 +0700

    adjusted requirements

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ from plotly import graph_objs as go
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-st.title('Stock Forecast App')
+st.title('Stock Forecast App - Banking University of Ho Chi Minh city')
 
 stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
-n_years = st.slider('Years of prediction:', 1, 4)
+n_years = st.slider('Years of prediction:', 1, 5)
 period = n_years * 365
 
 
